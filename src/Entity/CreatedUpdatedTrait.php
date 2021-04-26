5dd2823e7796e91d5682ce33c27572e8b6f7036f
MrGoodBytes <bytes@goodbytes.live>
2021-04-26 17:04:34 -0500

    CreatedUpdatedTrait

---
+++ src/Entity/CreatedUpdatedTrait.php
@@ -0,0 +1,79 @@
+<?php
+
+
+namespace Bytes\ResponseBundle\Entity;
+
+
+use DateTime;
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Trait CreatedUpdatedTrait
+ * @package Bytes\ResponseBundle\Entity
+ */
+trait CreatedUpdatedTrait
+{
+    /**
+     * @var DateTimeInterface
+     * @ORM\Column(type="datetime")
+     */
+    protected $createdAt;
+
+    /**
+     * @var DateTimeInterface
+     * @ORM\Column(type="datetime")
+     */
+    protected $updatedAt;
+
+    /**
+     * @return DateTimeInterface|null
+     */
+    public function getCreatedAt(): ?DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTimeInterface|null $createdAt
+     * @return $this
+     */
+    public function setCreatedAt(?DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setupDates(): self
+    {
+        $this->setCreatedAt(new DateTime());
+        if ($this->getUpdatedAt() == null) {
+            $this->setUpdatedAt(new DateTime());
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return DateTimeInterface|null
+     */
+    public function getUpdatedAt(): ?DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param DateTimeInterface|null $updatedAt
+     * @return $this
+     */
+    public function setUpdatedAt(?DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
