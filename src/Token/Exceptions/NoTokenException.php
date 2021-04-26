abee52ea0de8c545b3069502c3bee981c6e16575
MrGoodBytes <bytes@goodbytes.live>
2021-04-26 14:49:33 -0500

    Add NoTokenException

---
+++ src/Token/Exceptions/NoTokenException.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Bytes\ResponseBundle\Token\Exceptions;
+
+
+/**
+ * Class NoTokenException
+ * @package Bytes\ResponseBundle\Token\Exceptions
+ */
+class NoTokenException extends \Exception
+{
+
+}
