24fd3864a374897ef4ed2f781ec1f907088d7086
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2021-04-29 09:22:33 -0500

    Allow for overriding the add/remove parent indexes (#7)
    * Allow for overriding the add/remove parent indexes
    
    * Remove any extra indexes at the add/remove level as well

--- src/Objects/ConfigNormalizer.php
+++ src/Objects/ConfigNormalizer.php
@@ -11,45 +11,53 @@ namespace Bytes\ResponseBundle\Objects;
 class ConfigNormalizer
 {
     /**
-     * Normalizes the endpoint portion of the config, removing any indexes not in $endpoints, and adding any missing
-     * indexes from $endpoints
+     * Normalizes the endpoint portion of the config, removing any indexes not in $endpoints, adding any missing
+     * indexes from $endpoints, then adding any missing $addRemoveParents indexes, and removing any indexes not in
+     * $addRemoveParents (+redirects)
      * @param array $config
-     * @param string[] $endpoints
+     * @param string[] $endpoints = ['app', 'bot', 'eventsub_subscribe', 'login', 'slash', user']
+     * @param array $addRemoveParents = ['permissions', 'scopes']
      * @return array
      */
-    public static function normalizeEndpoints(array $config, array $endpoints)
+    public static function normalizeEndpoints(array $config, array $endpoints, array $addRemoveParents = ['permissions', 'scopes'])
     {
-        foreach($config['endpoints'] as $index => $endpoint) {
-            if(!in_array($index, $endpoints)) {
+        // Remove any $config['endpoints'] not in the $endpoints argument
+        foreach ($config['endpoints'] as $index => $endpoint) {
+            if (!in_array($index, $endpoints)) {
                 unset($config['endpoints'][$index]);
             }
         }
+
         foreach ($endpoints as $index) {
-            if(!isset($config['endpoints'][$index]))
-            {
+            if (!isset($config['endpoints'][$index])) {
                 $config['endpoints'][$index] = [];
             }
 
-            if(!isset($config['endpoints'][$index]['redirects']))
-            {
+            if (!isset($config['endpoints'][$index]['redirects'])) {
                 $config['endpoints'][$index]['redirects'] = [];
             }
-            if(!isset($config['endpoints'][$index]['redirects']['method']))
-            {
+            if (!isset($config['endpoints'][$index]['redirects']['method'])) {
                 $config['endpoints'][$index]['redirects']['method'] = 'route_name';
             }
-            foreach(['route_name', 'url'] as $redirects) {
+            foreach (['route_name', 'url'] as $redirects) {
                 if (!isset($config['endpoints'][$index]['redirects'][$redirects])) {
                     $config['endpoints'][$index]['redirects'][$redirects] = '';
                 }
             }
 
-            foreach(['permissions', 'scopes'] as $subIndex) {
-                if(!isset($config['endpoints'][$index][$subIndex])) {
+            // Remove any $config['endpoints'] children not in the $addRemoveParents (+redirects) argument
+            foreach ($config['endpoints'][$index] as $subIndex => $endpoint) {
+                if ($subIndex !== 'redirects' && !in_array($subIndex, $addRemoveParents)) {
+                    unset($config['endpoints'][$index][$subIndex]);
+                }
+            }
+
+            foreach ($addRemoveParents as $subIndex) {
+                if (!isset($config['endpoints'][$index][$subIndex])) {
                     $config['endpoints'][$index][$subIndex] = [];
                 }
-                foreach(['add', 'remove'] as $addRemove) {
-                    if(!isset($config['endpoints'][$index][$subIndex][$addRemove])) {
+                foreach (['add', 'remove'] as $addRemove) {
+                    if (!isset($config['endpoints'][$index][$subIndex][$addRemove])) {
                         $config['endpoints'][$index][$subIndex][$addRemove] = [];
                     }
                 }
