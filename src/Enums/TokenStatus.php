e60ea3d661c5044b49e305578ff912b48345a330
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2021-05-10 10:11:24 -0500

    Add TokenStatus enum (#11)

---
+++ src/Enums/TokenStatus.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Bytes\ResponseBundle\Enums;
+
+
+use BadMethodCallException;
+use Bytes\EnumSerializerBundle\Enums\Enum;
+use TypeError;
+
+/**
+ * Class TokenStatus
+ * @package Bytes\ResponseBundle\Enums
+ *
+ * @method static self granted()
+ * @method static self refreshed()
+ * @method static self expired()
+ * @method static self revoked()
+ */
+class TokenStatus extends Enum
+{
+    /**
+     * @param TokenStatus|string $status
+     * @return bool
+     */
+    public static function isActive(TokenStatus|string $status): bool
+    {
+        if (is_string($status)) {
+            try {
+                $status = TokenStatus::make($status);
+            } catch (BadMethodCallException | TypeError $exception) {
+                return false;
+            }
+        }
+        return $status->equals(TokenStatus::granted(), TokenStatus::refreshed());
+    }
+}
