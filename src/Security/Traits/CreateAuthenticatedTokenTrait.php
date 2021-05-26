56c50cd08e4a1b49c8bff3980b95fd82f09c9afa
MrGoodBytes <bytes@goodbytes.live>
2021-05-26 12:45:24 -0500

    Guard is deprecated in Symfony 5.3

--- src/Security/Traits/CreateAuthenticatedTokenTrait.php
+++ src/Security/Traits/CreateAuthenticatedTokenTrait.php
@@ -5,9 +5,9 @@ namespace Bytes\ResponseBundle\Security\Traits;
 
 
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
+use Symfony\Component\Security\Http\Authenticator\Token\PostAuthenticationToken;
 
 /**
  * Trait CreateAuthenticatedTokenTrait
@@ -16,19 +16,19 @@ use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface
 trait CreateAuthenticatedTokenTrait
 {
     /**
-     * Creates a PostAuthenticationGuardToken with the entity id of the token attached as attribute accessToken
+     * Creates a PostAuthenticationToken with the entity id of the token attached as attribute accessToken
      *
      * @param PassportInterface $passport
      * @param string $firewallName
      *
-     * @return PostAuthenticationGuardToken
+     * @return PostAuthenticationToken
      */
     public function createAuthenticatedToken(PassportInterface $passport, string $firewallName): TokenInterface
     {
         if (!$passport instanceof UserPassportInterface) {
             throw new \LogicException(sprintf('Passport does not contain a user, overwrite "createAuthenticatedToken()" in "%s" to create a custom authenticated token.', static::class));
         }
-        $token = new PostAuthenticationGuardToken($passport->getUser(), $firewallName, $passport->getUser()->getRoles());
+        $token = new PostAuthenticationToken($passport->getUser(), $firewallName, $passport->getUser()->getRoles());
         $token->setAttribute('accessToken', $passport->getAttribute('accessToken')?->getId());
         $token->setAttribute('tokenIdentifier', $passport->getAttribute('tokenIdentifier'));
         return $token;
