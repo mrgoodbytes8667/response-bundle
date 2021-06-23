2d44333f479d6536fe74564048e2d678048800cd
MrGoodBytes <bytes@goodbytes.live>
2021-06-22 22:25:01 -0500

    Added gif and webp content types

--- src/Enums/ContentType.php
+++ src/Enums/ContentType.php
@@ -10,8 +10,10 @@ use Bytes\EnumSerializerBundle\Enums\Enum;
  * Class ContentType
  * @package Bytes\ResponseBundle\Enums
  *
+ * @method static self imageGif()
  * @method static self imageJpg()
  * @method static self imagePng()
+ * @method static self imageWebP()
  *
  * @method static self json()
  */
@@ -23,8 +25,10 @@ class ContentType extends Enum
     protected static function values(): array
     {
         return [
+            'imageGif' => 'image/gif',
             'imageJpg' => 'image/jpeg',
             'imagePng' => 'image/png',
+            'imageWebP' => 'image/webp',
             'json' => 'application/json',
         ];
     }
