c8c6ca70d57340354e4add7a49f08a76b4e13027
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2022-02-09 11:04:48 -0600

    Remove PHP 8.0 test now that project is 8.1+ (#39)

--- Tests/Objects/ComparableDateIntervalTest.php
+++ Tests/Objects/ComparableDateIntervalTest.php
@@ -115,15 +115,4 @@ class ComparableDateIntervalTest extends TestCase
         $this->expectException(Exception::class);
         new ComparableDateInterval('abc123');
     }
-
-    /**
-     * @requires PHP < 8.1
-     * @return void
-     */
-    public function testInvalidConstructorPhp8()
-    {
-        $this->expectException(Exception::class);
-        $this->expectExceptionMessage('DateInterval::__construct(): Unknown or bad format');
-        new ComparableDateInterval('abc123');
-    }
 }
