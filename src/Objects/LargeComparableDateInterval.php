5a4974bed33466d57567e61aee3317017d62cec8
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2022-10-27 10:05:59 -0500

    Updated Date Intervals to use the days parameter when available (#59)
    Cleaned up tests

--- src/Objects/LargeComparableDateInterval.php
+++ src/Objects/LargeComparableDateInterval.php
@@ -6,6 +6,7 @@ use DateInterval;
 
 /**
  * Unlike \Bytes\ResponseBundle\Objects\ComparableDateInterval this method assumes a 30 day month and a 365 day year
+ * unless the days variable is set [the DateInterval object was created by DateTimeImmutable::diff() or DateTime::diff()]
  */
 class LargeComparableDateInterval extends ComparableDateInterval
 {
@@ -15,7 +16,7 @@ class LargeComparableDateInterval extends ComparableDateInterval
      */
     protected static function parseYears(DateInterval $interval): int
     {
-        if ($interval->y > 0) {
+        if (!static::hasDaysVariable($interval) && $interval->y > 0) {
             return ($interval->y * 365 * 86400);
         }
 
@@ -28,7 +29,7 @@ class LargeComparableDateInterval extends ComparableDateInterval
      */
     protected static function parseMonths(DateInterval $interval): int
     {
-        if ($interval->m > 0) {
+        if (!static::hasDaysVariable($interval) && $interval->m > 0) {
             return ($interval->m * 30 * 86400);
         }
 
