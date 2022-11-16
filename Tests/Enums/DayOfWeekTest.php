eacf237b77ed92756fff1ed8b43fc4e32defd0d6
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2022-11-16 10:49:09 -0600

    Add DayOfWeek enum (#63)

---
+++ Tests/Enums/DayOfWeekTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Bytes\ResponseBundle\Tests\Enums;
+
+use Bytes\EnumSerializerBundle\PhpUnit\EnumAssertions;
+use Bytes\ResponseBundle\Enums\DayOfWeek;
+use Generator;
+use PHPUnit\Framework\TestCase;
+
+class DayOfWeekTest extends TestCase
+{
+    /**
+     * @dataProvider provideAll
+     * @param DayOfWeek $enum
+     * @param int $value
+     * @return void
+     */
+    public function testAll(DayOfWeek $enum, int $value): void
+    {
+        EnumAssertions::assertIsEnum($enum);
+        EnumAssertions::assertEqualsEnum($enum, $value);
+        EnumAssertions::assertSameEnumValue($enum, $value);
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideAll(): Generator
+    {
+        foreach (DayOfWeek::cases() as $enum) {
+            yield $enum->value => ['enum' => $enum, 'value' => $enum->value];
+        }
+    }
+
+    /**
+     * @dataProvider provideAll
+     * @param DayOfWeek $enum
+     * @param int $value
+     * @return void
+     */
+    public function testTryFromSuccessful(DayOfWeek $enum, int $value): void
+    {
+        EnumAssertions::assertSameEnum($enum, DayOfWeek::tryFrom($value));
+    }
+
+    /**
+     * @return void
+     */
+    public function testTryFromUnsuccessful(): void
+    {
+        $this->assertNull(DayOfWeek::tryFrom(0));
+        $this->assertNull(DayOfWeek::tryFrom(8));
+    }
+}
---
+++ src/Enums/DayOfWeek.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bytes\ResponseBundle\Enums;
+
+
+use Bytes\EnumSerializerBundle\Enums\BackedEnumInterface;
+use Bytes\EnumSerializerBundle\Enums\BackedEnumTrait;
+
+enum DayOfWeek: int implements BackedEnumInterface
+{
+    use BackedEnumTrait;
+
+    case SUNDAY = 7;
+    case MONDAY = 1;
+    case TUESDAY = 2;
+    case WEDNESDAY = 3;
+    case THURSDAY = 4;
+    case FRIDAY = 5;
+    case SATURDAY = 6;
+}
