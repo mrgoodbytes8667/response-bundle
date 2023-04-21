c3c67fc29671d091b41e6f4a69ee2d02b218d2ef
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2023-04-21 09:31:28 -0500

    Replace deprecated core security class with newer security bundle class (#71)

--- Tests/Security/Traits/SecurityTraitTest.php
+++ Tests/Security/Traits/SecurityTraitTest.php
@@ -5,7 +5,7 @@ namespace Bytes\ResponseBundle\Tests\Security\Traits;
 use Bytes\ResponseBundle\Security\SecurityTrait;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
