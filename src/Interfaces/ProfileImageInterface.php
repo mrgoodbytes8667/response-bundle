6b21b686935cde82f73414558bc318a9325bd95c
MrGoodBytes <bytes@mrgoodbytes.dev>
2023-05-08 14:18:51 -0500

    Add ProfileImageInterface

---
+++ src/Interfaces/ProfileImageInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bytes\ResponseBundle\Interfaces;
+
+/**
+ * Interface ProfileImageInterface
+ * @package Bytes\ResponseBundle\Interfaces
+ */
+interface ProfileImageInterface
+{
+    /**
+     * @return string|null
+     */
+    public function getProfileImage(): ?string;
+}
