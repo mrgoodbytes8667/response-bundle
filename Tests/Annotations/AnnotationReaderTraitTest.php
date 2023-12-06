141d719dc049d7dc7d90be2b7dc535f494bfd1c8
MrGoodBytes <bytes@mrgoodbytes.dev>
2023-12-06 12:20:42 -0600

    Expect deprecation

--- Tests/Annotations/AnnotationReaderTraitTest.php
+++ Tests/Annotations/AnnotationReaderTraitTest.php
@@ -5,20 +5,22 @@ namespace Bytes\ResponseBundle\Tests\Annotations;
 use Bytes\ResponseBundle\Annotations\AnnotationReaderTrait;
 use Doctrine\Common\Annotations\AnnotationReader;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
 
 /**
- * Class AnnotationReaderTraitTest
- * @package Bytes\ResponseBundle\Tests\Annotations
+ * @deprecated Annotation support will be dropped in v6.0.0
  */
 class AnnotationReaderTraitTest extends TestCase
 {
+    use ExpectDeprecationTrait;
+
     /**
-     *
+     * @group legacy
      */
     public function testSetReader()
     {
         $mock = $this->getMockForTrait(AnnotationReaderTrait::class);
-
+        $this->expectDeprecation('Since mrgoodbytes8667/response-bundle 5.3.0: Annotation support will be dropped in v6.0.0.');
         self::assertNotNull($mock->setReader(new AnnotationReader()));
     }
 }
